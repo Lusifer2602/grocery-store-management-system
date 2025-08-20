4d2b27d0c9409dd919e40b4300bf8d7f013a4bdd
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-08-20 19:21:10 +0000

    nothing here just signing off for the night

--- cli/pranav.py
+++ cli/pranav.py
@@ -1,10 +1,16 @@
 print("Welcome To Our Grocery Store")
+def order():
+    
 def signin():
     login=input("Enter your email/mobile : ").lower()
     #add code where this place looks up the user in database then asks for password and also look for their pass in user database
     #later add password decryption here
     answer=input("Enter password : ")
     print(f"Hi, {username}")
+    choice=int(input("What would you like today?"
+           "\n1.Order new items\n2.View your previous orders\n3.Just go through items in our stock : "))
+    if choice==1:
+        order()
 
 
 while True:
