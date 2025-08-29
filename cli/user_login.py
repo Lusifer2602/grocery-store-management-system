9e2503913a45e5442e800bf0e656d9885b9d77b2
jerry74114 <anshubeniwal669@gmail.com>
2025-08-29 20:22:54 +0530

    change 1.5

--- cli/user_login.py
+++ cli/user_login.py
@@ -1,4 +1,5 @@
 from function import *
+
 print_banner("GROCERRY MANAGMENT SYSTEM")
 typing_effect("Welcome.....")
 print("\n")
@@ -6,8 +7,27 @@ msg = """Hii Please select From given options:--
 1. Admin Login
 2. Sign In 
 3. Log In 
+4. Exit
 """
 typing_effect(msg,0.06)
-opt = int(input(":"))
-if opt ==1:
-    sign_in()
+while True:
+    opt = int(input(":"))
+    if opt ==1:
+        # admin_pg()
+        print("admin page temp....")
+        break
+    elif opt == 2:
+        sign_in()
+        break
+    elif opt ==3:
+        # log_in()
+        print("Log in page temp......")
+        break
+    elif opt == 4:
+        typing_effect("Thanks for using our app ")
+        print("\n")
+        typing_effect("Press enter to exit")
+
+    else:
+        typing_effect("Please enter correct options......")
+        print("\n")
