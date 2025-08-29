277d0225b0d82677f21414adbb1fcb2a2cdfedd6
jerry74114 <anshubeniwal669@gmail.com>
2025-08-29 19:24:35 +0530

    Change 1.3

--- cli/sign_in.py
+++ cli/sign_in.py
@@ -20,36 +20,36 @@ def print_banner(text):
 
 
 # Code Area
+def sign_in ():
+    os.system("cls")
 
-os.system("cls")
+    print_banner("NEW   USER") 
 
-print_banner("NEW   USER") 
+    typing_effect("Press Enter to Continue.....", 0.05)
+    input()
+    os.system("cls")
 
-typing_effect("Press Enter to Continue.....", 0.05)
-input()
-os.system("cls")
+    new_user_name = input("Enter your Name\n:")
+    while True:
+        new_user_email = input("Enter you Email \n:")
+        if new_user_email.endswith("@gmail.com"):
+            break
+        else:
+                print("Please Enter Valid Email....")
+    while True:
+        new_user_pass = input("Enter your Password\n:")
+        if len(new_user_pass) <8:
+            print("Please Enter Atleast 8 Character ")
+        else:
+            break
+    os.system("cls")
+    hii_msg = f'Hi {new_user_name}\n'
+    info = f"""
+    Name > {new_user_name} 
+    Email > {new_user_email}
+    Password > {new_user_pass} """
 
-new_user_name = input("Enter your Name\n:")
-while True:
-    new_user_email = input("Enter you Email \n:")
-    if new_user_email.endswith("@gmail.com"):
-        break
-    else:
-        print("Please Enter Valid Email....")
-while True:
-    new_user_pass = input("Enter your Password\n:")
-    if len(new_user_pass) <8:
-        print("Please Enter Atleast 8 Character ")
-    else:
-        break
-os.system("cls")
-hii_msg = f'Hi {new_user_name}\n'
-info = f"""
-Name > {new_user_name} 
-Email > {new_user_email}
-Password > {new_user_pass} """
-
-typing_effect(hii_msg,0.05)
-typing_effect("Please Verify the Information:-",0.05)
-typing_effect(info,0.06)
-
+    typing_effect(hii_msg,0.05)
+    typing_effect("Please Verify the Information:-",0.05)
+    typing_effect(info,0.06)
+    return [new_user_email,new_user_name,new_user_pass]
