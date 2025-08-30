6d1af7be38f331323fdb012c07881ae41b9dc25a
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-08-30 11:12:01 +0000

    working on conecting sql to signup page

--- cli/signup.py
+++ cli/signup.py
@@ -1,11 +1,9 @@
-# Module Area
+import sqlite3 as sq
 import sys
 import time
 import pyfiglet
 import shutil
 import os
-# Function Area
-
 def typing_effect(text, delay=0.05):
     for char in text:
         sys.stdout.write(char)   
@@ -17,41 +15,56 @@ def print_banner(text):
     banner = pyfiglet.figlet_format(text)
     for line in banner.split("\n"):
         print(line.center(columns))
+con=sq.connect('info.db')
+c=con.cursor()
+
+def signup():
+    os.system("clear")
+
+    print_banner("Sign Up for\nNew Account") 
+
+    typing_effect("Press Enter to Continue.....", 0.05)
+    input()
+
+    new_user_name = input("Enter your Name : ")
+    while True:
+        new_user_email = input("Enter you Email :")
+        if new_user_email.endswith("@gmail.com") and len(new_user_email)>len("@gmail.com"):
+            break
+        else:
+                print("Please Enter A Valid Email....")
+    while True:
+        passw= input("Enter New Password :")
+        
+        if len(passw) <8:
+            print("Please Enter Atleast 8 Characters ")
+        else:
+            break
+        repass=input("Re-Enter your Password : ")
+        while True:
+            if passw==repass:
+                break
+            else: print("Passwords don't match try again : ")
+
+    os.system("clear")
+    hii_msg = f'Hi {new_user_name}\n'
+    info = f"""
+    Name > {new_user_name} 
+    Email > {new_user_email}
+    Password > {passw} """
+
+    typing_effect(hii_msg,0.05)
+    typing_effect("Please Verify Your Information : \n",0.05)
+    typing_effect(info,0.04)
+    choice=input("\nEnter 'y' if the information is correct\nand 'n' to re-setup your account : ").lower()
+    if choice=='y' or choice === 'yes':
+    c.execute("""CREATE TABLE IF NOT EXISTS users(
+              name TEXT,
+              mail text PRIMARY,
+              passw 
+              )""")
+        return [new_user_email,new_user_name,passw]
 
 
-# Code Area
 
-os.system("cls")
-
-print_banner("Welcome to our GMS\nlet's setup your account") 
-
-typing_effect("Press Enter to Continue.....", 0.05)
-input()
-os.system("cls")
-
-new_user_name = input("Enter your Name : ")
-while True:
-    new_user_email = input("Enter you Email : ")
-    if new_user_email.endswith("@gmail.com"):
-        break
-    else:
-        print("Please Enter Valid Email....")
-while True:
-    new_user_pass = input("Enter Password :")
-    if len(new_user_pass) <8:
-        print("Please Enter Atleast 8 Characters ")
-    else:
-        break
-os.system("cls")
-typing_effect("Please revise your info before we save it to the database"
-              "\nPress y moving ahead & n to re-setup your account : ")
-input()
-info = f"""
-Name > {new_user_name} 
-Email > {new_user_email}
-Password > {new_user_pass} """
-
-typing_effect(hii_msg,0.05)
-typing_effect("Please Verify the Information:-",0.05)
-typing_effect(info,0.06)
-print("\n")
+# User_login page
