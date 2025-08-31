d49a8aea432ace4d5132fe69aca7d37a0b0539d8
jerry74114 <anshubeniwal669@gmail.com>
2025-08-31 21:21:01 +0530

    Change2.0

--- cli/function.py
+++ cli/function.py
@@ -4,6 +4,7 @@ import time
 import pyfiglet
 import shutil
 import os
+
 # Function Area
 
 def typing_effect(text, delay=0.05):
@@ -17,41 +18,15 @@ def print_banner(text):
     banner = pyfiglet.figlet_format(text)
     for line in banner.split("\n"):
         print(line.center(columns))
-# sign in page 
-def sign_in ():
-    os.system("cls")
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
-    print_banner("NEW   USER") 
 
-    typing_effect("Press Enter to Continue.....", 0.05)
-    input()
-    os.system("cls")
+#Login Functions
 
-    new_user_name = input("Enter your Name\n:")
-    while True:
-        new_user_email = input("Enter you Email \n:")
-        if new_user_email.endswith("@gmail.com") and len(new_user_email)>len("@gmail.com"):
-            break
-        else:
-                print("Please Enter Valid Email....")
-    while True:
-        new_user_pass = input("Enter your Password\n:")
-        if len(new_user_pass) <8:
-            print("Please Enter Atleast 8 Character ")
-        else:
-            break
-    os.system("cls")
-    hii_msg = f'Hi {new_user_name}\n'
-    info = f"""
-    Name > {new_user_name} 
-    Email > {new_user_email}
-    Password > {new_user_pass} """
-
-    typing_effect(hii_msg,0.05)
-    typing_effect("Please Verify the Information:-",0.05)
-    typing_effect(info,0.06)
-    return [new_user_email,new_user_name,new_user_pass]
-# User_login page
 def user_login():
     print_banner("GROCERRY MANAGMENT SYSTEM")
     typing_effect("Welcome.....")
@@ -66,15 +41,15 @@ def user_login():
     while True:
         opt = int(input(":"))
         if opt ==1:
-            os.system("cls")
+            clear_screen()
             admin_login()
             break
         elif opt == 2:
-            os.system("cls")
+            clear_screen()
             sign_in()
             break
         elif opt ==3:
-            os.system("cls")
+            clear_screen()
             log_in()
             break
         elif opt == 4:
@@ -102,10 +77,88 @@ def admin_login():
         if admin_id == "abc" and admin_pass == "2345":
             print("Welcome.., Press Enter to Continue")
             input()
-            # admin_pg()
+            clear_screen()
+            admin_page()
             break
         else:
             print("Wrong ID and PASS please try again ")
+
+def admin_page():
+    print_banner("ADMIN")
+    msg = '''OPT:- 
+1. View Users list
+2. Remove User
+3. Items List
+4. View Bills history
+5. Exit'''
+    typing_effect(msg,0.04)
+    while True:
+        opt = int(input("\n:"))
+
+        if opt == 1:
+            clear_screen()
+            print_banner("User List")
+            print("Pandu sql ki list print krvani h yha pe tuje iska bhi alg se simple page bna na h fuck... ")
+            #user_list_page()
+            input()
+        elif opt == 2:
+            clear_screen()
+            print_banner('Remove User')
+            print("yha pe bhai sql use hoga tuje user remove ka krna h iska bhi page bna na pde ga fuckk....")
+            #user_remove_page()
+            input()
+        elif opt ==3:
+            clear_screen()
+            print_banner("Items List")
+            print("Items list...")
+            input()
+            #item list()
+        elif opt==4:
+            clear_screen()
+            print_banner("Bills History")
+            input()
+            #bills_history page()
+        elif opt ==5:
+            clear_screen()
+            user_login()
+        else:
+            print("Please Enter Valid options..")
+        
+def sign_in ():
+    clear_screen()
+    print_banner("NEW   USER") 
+
+    typing_effect("Press Enter to Continue.....", 0.05)
+    input()
+    clear_screen()
+
+    new_user_name = input("Enter your Name\n:")
+    while True:
+        new_user_email = input("Enter you Email \n:")
+        if new_user_email.endswith("@gmail.com") and len(new_user_email)>len("@gmail.com"):
+            break
+        else:
+                print("Please Enter Valid Email....")
+    while True:
+        new_user_pass = input("Enter your Password\n:")
+        if len(new_user_pass) <8:
+            print("Please Enter Atleast 8 Character ")
+        else:
+            break
+            clear_screen()
+    hii_msg = f'Hi {new_user_name}\n'
+    info = f"""
+    Name > {new_user_name} 
+    Email > {new_user_email}
+    Password > {new_user_pass} """
+
+    typing_effect(hii_msg,0.05)
+    typing_effect("Please Verify the Information:-",0.05)
+    typing_effect(info,0.06)
+    return [new_user_email,new_user_name,new_user_pass]
+
+
+
 
 def log_in():
     print_banner("LOGIN")
@@ -113,3 +166,4 @@ def log_in():
 
     print("Please Enter Login Id & Pass")
     input("Enter to exit for now")
+
