2d5be709b5f2a642fb86c759e998c81281aea6e4
Lusifer2602 <2602pranav@gmail.com>
2025-09-03 01:04:42 +0530

    started workig on admin panel

---
+++ cli/admin.py
@@ -0,0 +1,15 @@
+from fuctios import * #shall import all the cotets of fuctios here so i ca use to call them
+
+def admi():
+    clrscr()
+    prit_baer("Hello Admin")
+    typig_effect("What would you like to do today? : "
+                  "\n1. Show Uers 2.Edit Users 3. Show Items 4. Edit Items"
+                 "Your choice : "
+                 )
+    choice=iput("")
+    while True:
+        if choice==1:
+            
+
+admi() 
